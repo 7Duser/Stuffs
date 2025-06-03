9c65037d02d504b79930527184b7d020a82192aa
dr_breen <37158907+7Duser@users.noreply.github.com>
2025-06-03 23:54:20 +0200

    more cleanup

--- randomobf.lua
+++ randomobf.lua
@@ -1,19 +1,18 @@
 return (function(...)
-	local a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p = table.unpack, bit32.rshift, buffer.readstring, string.char, buffer.readu32, table.move, buffer.readu8, typeof, buffer.readf64, table.pack, getfenv, tonumber, buffer.readi32, bit32.band, buffer.fromstring, setmetatable;
 	local function q(r)
-		return n(r, 255);
+		return bit32.band(r, 255);
 	end;
 	local function r(s)
-		return n(b(s, 8), 255);
+		return bit32.band(bit32.rshift(s, 8), 255);
 	end;
 	local function s(t)
-		return n(b(t, 16), 255);
+		return bit32.band(bit32.rshift(t, 16), 255);
 	end;
 	local function t(u)
-		return n(b(u, 24), 255);
+		return bit32.band(bit32.rshift(u, 24), 255);
 	end;
 	local function u(v)
-		local w = b(v, 16);
+		local w = bit32.rshift(v, 16);
 		if w >= 32768 then
 			return w - (65536);
 		else
@@ -113,66 +112,66 @@ return (function(...)
 		[40] = type,
 		[21] = math.pow
 	}, "736b69646675736361746f7220626266393333310100000000000f00000001000000000000000002050000007072696e7400021400000057656c636f6d6520746f204959204c6f61646572020700000067657466656e760002090000005363726970744b657902050000006572726f7200021b00000067657466656e7628292e5363726970744b6579206973206e696c21020c0000001951764a2f0e5f081a3e5a18020b0000006a3a1f2e497b2c6b7b4a35021f00000067657466656e7628292e5363726970744b657920697320696e76616c696421025e0000001e214f46260659252b1d48330659303a004c630611333a0a2c491a0d22381c1b6c5d0b373f41462a0611232a1a5226001a39261b442d06573527020e06161e3301360e2a1c1f3f260655260b1033240b0e2e130a222d1d0e301d0c242b0a020700000056486f214372792400000041000000400000000c0102000000104005020300150102010c02050000004040150201020f01025406000000190105000c020800000070400503090015020201160001000602000005030a0005040b00150203021b010600020000000c0208000000704005030c0015020201160001000c020200000010400603000005040d0005050e00150303001502000116000100010000000000000000000c00000001000000000000f03f0206000000737472696e670204000000627974650002050000006269743332020400000062786f72000204000000636861720002050000007461626c650206000000636f6e636174002a00000035020000000000003403010004060100340400000405010038041e002809060025080903270708004a2900050600000006090000060a06000c08030000081080150803024a29010507000000060a0100060b07000c09030000081080150903024a20080509000000060c0800060d09000c0b060000144080150b0302492a0b020c0a0800001c1080150a02020e0a02063904e2ff0c040b000028908006050200150402001604000000000000";
-	local function x()
-		local y, z = o(w:gsub("..", function(y)
-			return d(l(y, 16));
+	local function Deserialize()
+		local y, z = buffer.fromstring(w:gsub("..", function(y)
+			return string.char(tonumber(y, 16));
 		end)), 20;
 		local function A()
 			local B, C, D = {}, {}, {};
 			local E, F = "", {
-				[9] = p({}, {
+				[9] = setmetatable({}, {
 					__index = B
 				}),
-				[31] = p({}, {
+				[31] = setmetatable({}, {
 					__index = C
 				}),
-				[8] = p({}, {
+				[8] = setmetatable({}, {
 					__index = D
 				})
 			};
-			F[126] = m(y, z);
-			z = z + 84 - 80;
+			F[126] = buffer.readi32(y, z);
+			z = z + 4;
 			while true do
-				local G = g(y, z);
-				z = z + 77 + (-76);
+				local G = buffer.readu8(y, z);
+				z = z + 1;
 				if G ~= 0 then
-					E = E .. d(G);
+					E = E .. string.char(G);
 				else
 					break;
 				end;
 			end;
 			F[6] = E;
-			local G = g(y, z);
+			local G = buffer.readu8(y, z);
 			F[7] = G;
-			z = z + 5 - 4;
-			local H = e(y, z);
+			z = z + 1;
+			local H = buffer.readu32(y, z);
 			z = z + 4;
 			for I = 0, H - 1 do
-				local J = g(y, z);
+				local J = buffer.readu8(y, z);
 				z = z + 1;
 				if J ~= 0 then
 					if J ~= 1 then
 						if J == 2 then
-							local K = e(y, z);
-							z = z + 180 + (-176);
-							B[I] = c(y, z, K);
+							local K = buffer.readu32(y, z);
+							z = z + 4;
+							B[I] = buffer.readstring(y, z, K);
 							z = z + K;
 						else
 							B[I] = {};
 						end;
 					else
-						B[I] = i(y, z);
-						z = z + 328 - 320;
+						B[I] = buffer.readf64(y, z);
+						z = z + 8;
 					end;
 				else
 					B[I] = nil;
 				end;
 			end;
-			local I = e(y, z);
-			z = z + 344 - 340;
+			local I = buffer.readu32(y, z);
+			z = z + 4;
 			for J = 0, I - (1) do
-				local K = e(y, z);
-				D[J] = p({}, {
+				local K = buffer.readu32(y, z);
+				D[J] = setmetatable({}, {
 					__index = function(L, M)
 						if M ~= 0 then
 							if M ~= 90 then
@@ -201,13 +200,13 @@ return (function(...)
 				});
 				z = z + 4;
 			end;
-			local J = e(y, z);
+			local J = buffer.readu32(y, z);
 			z = z + 4;
 			for K = 1, J do
 				local L = A();
 				C[L[126]] = L;
 			end;
-			return p({}, {
+			return setmetatable({}, {
 				__iter = function()
 					while true do
 					end;
@@ -217,12 +216,12 @@ return (function(...)
 		end;
 		return A();
 	end;
-	local function y(z, A, B)
+	local function Wrap(z, A, B)
 		return function(...)
 			local C, D, E, F, G, H, I, J = z[9], {}, 1, z[8], 0, {
 				...
 			}, z[31], {};
-			local K = p({}, {
+			local K = setmetatable({}, {
 				__index = function(K, L)
 					local M = J[L];
 					local N = M[1];
@@ -300,12 +299,12 @@ return (function(...)
 																				else
 																					local N, O, P = L[90], F[G + 1][192], L[4] - (1);
 																					G = G + 1;
-																					local Q = b(O, 30);
+																					local Q = bit32.rshift(O, 30);
 																					if Q > 1 then
-																						local R = B[C[n(b(O, 20), 1023)]];
-																						local S = R[C[n(b(O, 10), 1023)]];
+																						local R = B[C[bit32.band(bit32.rshift(O, 20), 1023)]];
+																						local S = R[C[bit32.band(bit32.rshift(O, 10), 1023)]];
 																						if Q > 2 then
-																							D[N] = S[C[n(O, 1023)]];
+																							D[N] = S[C[bit32.band(O, 1023)]];
 																						else
 																							D[N] = S;
 																						end;
@@ -324,14 +323,14 @@ return (function(...)
 																	else
 																		local N, O, P = L[2], F[G + (1)][192], L[90];
 																		D[P] = D[N][C[O]];
-																		G = G + 71 - 70;
+																		G = G + 1;
 																	end;
 																else
 																	local N, O = L[90], L[4];
 																	local P = C[O];
 																	local Q = I[P];
 																	for R = 0, Q[7] - (1) do
-																		G = G + 47 + (-46);
+																		G = G + 1;
 																		local S = F[G];
 																		local T = S[90];
 																		J[R] = {
@@ -343,18 +342,18 @@ return (function(...)
 																			J[R][2] = S[2];
 																		end;
 																	end;
-																	D[N] = y(Q, K, B);
+																	D[N] = Wrap(Q, K, B);
 																end;
 															else
 																local N, O, P = L[206], L[90], L[2];
 																local Q = F[G + N + (1)][192];
 																local R, S, T = r(Q), t(Q) - (1), v[O];
-																local U = j(T(D[P]));
+																local U = table.pack(T(D[P]));
 																if S == (-1) then
 																	E = R + (#U);
 																	S = #U;
 																end;
-																f(U, 1, S, R, D);
+																table.move(U, 1, S, R, D);
 																G = G + N + (1);
 															end;
 														else
@@ -364,7 +363,7 @@ return (function(...)
 													else
 														local N = L[90];
 														E = E + N;
-														f(H, 1, N, 0, D);
+														table.move(H, 1, N, 0, D);
 													end;
 												else
 													local N, O, P = L[206], L[90], L[2];
@@ -373,7 +372,7 @@ return (function(...)
 											else
 												local N = L[90];
 												D[N] = {};
-												G = G + 68 - 67;
+												G = G + 1;
 											end;
 										else
 											local N, O = L[2] - (1), L[90];
@@ -384,7 +383,7 @@ return (function(...)
 												P = O + N;
 											end;
 											P = P - 1;
-											return a(D, O, P);
+											return table.unpack(D, O, P);
 										end;
 									else
 										local N, O = L[2], L[90];
@@ -399,41 +398,39 @@ return (function(...)
 								end;
 							else
 								local N, O, P, Q = F[G + (1)][192], L[90], L[206], L[2];
-								G = G + 81 - 80;
+								G = G + 1;
 								local R = F[G + P][192];
 								G = G + P;
 								local S, T, U = t(R) - (1), v[O], r(R);
-								local V = j(T(D[Q], D[N]));
+								local V = table.pack(T(D[Q], D[N]));
 								if S == (-1) then
 									E = U + (#V);
 									S = #V;
 								end;
-								f(V, 1, S, U, D);
+								table.move(V, 1, S, U, D);
 							end;
 						else
 							local N, O, P = L[90], L[2], L[206] - 1;
 							local Q = D[N];
-							if h(Q) ~= "function" then
-							end;
 							local R;
 							if O == 0 then
 								R = E;
 							else
 								R = N + O;
 							end;
-							local S = j(Q(a(D, N + (1), R - (1))));
+							local S = table.pack(Q(table.unpack(D, N + (1), R - (1))));
 							if P == (-1) then
 								E = N + (#S);
 								P = #S;
 							end;
-							f(S, 1, P, N, D);
+							table.move(S, 1, P, N, D);
 						end;
 					else
 						local N, O, P = F[G + (1)][192], L[90], L[4];
 						if D[O] == D[N] then
 							G = G + P;
 						else
-							G = G + 80 - 79;
+							G = G + 1;
 						end;
 					end;
 				else
@@ -451,5 +448,5 @@ return (function(...)
 			end;
 		end;
 	end;
-	return (y(x(), {}, k()))(...);
+	return (Wrap(Deserialize(), {}, getfenv()))(...);
 end)();
